9235c6687de8c67be54e098281d3222611f2afaf
MPdoor1 <107142603+MPdoor1@users.noreply.github.com>
2022-11-21 20:10:12 -0500

    TEST TEST

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -29,6 +29,7 @@ namespace Gaines_Opus_Institute_Current.Pages
         [BindProperty]
         public User user { get; set; } = default!;
 
+        //TESTTEST
         public IActionResult OnGet()
         {
             var userRedirect = User != null && User.Identity.IsAuthenticated;
