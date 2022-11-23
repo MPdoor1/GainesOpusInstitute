3677ff3d5fd5a5c09fa7e9e031fc98a54a200ceb
MPdoor1 <107142603+MPdoor1@users.noreply.github.com>
2022-11-23 18:47:03 -0500

    Fixed Logged in Navbar and Logout Button

---
+++ Pages/PagesLoggedIn/profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gaines_Opus_Institute_Current.Pages.PagesLoggedIn
+{
+    public class profileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
